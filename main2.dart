fa9480ebb46e537306eb1b2cb36a586d790e13b3
Elisheato <49386065+elisheato-master@users.noreply.github.com>
2025-03-12 11:37:02 +0800

    Create main2.dart

---
+++ main2.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'screens/login_screen.dart';
+import 'services/auth_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AuthService(),
+      child: MaterialApp(
+        title: 'Flutter Auth App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const LoginScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
